f4099db9f6215a99062dfbba66beb846c961b565
Adam Gustavsson <adam.gustavsson@gmail.com>
2025-10-03 08:34:13 +0200

    restored the noxfile

--- noxfile.py
+++ noxfile.py
@@ -12,10 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import nox
 import os
 import pathlib
-
-import nox
 
 PYTHON_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 

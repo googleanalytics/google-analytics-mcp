175bbca04ee6fdbe893705ee67e24b3bc26e707d
Carl Koblavie <carl@adaly.ai>
2025-10-09 14:41:09 +0000

    server mcp over http transport

--- analytics_mcp/coordinator.py
+++ analytics_mcp/coordinator.py
@@ -18,7 +18,37 @@ The singleton allows other modules to register their tools with the same MCP
 server using `@mcp.tool` annotations, thereby 'coordinating' the bootstrapping
 of the server.
 """
-from mcp.server.fastmcp import FastMCP
+#from mcp.server.fastmcp import FastMCP
 
-# Creates the singleton.
-mcp = FastMCP("Google Analytics Server")
+
+import os
+from fastmcp import FastMCP
+from fastmcp.server.auth.providers.jwt import JWTVerifier
+from analytics_mcp.custom_header_middleware import CustomHeaderMiddleware
+
+# Load Azure AD configuration
+tenant_id = os.environ.get("AZURE_TENANT_ID")
+client_id = os.environ.get("AZURE_CLIENT_ID")
+
+
+if tenant_id and client_id:
+    try:
+        auth = JWTVerifier(
+            jwks_uri=f"https://login.microsoftonline.com/{tenant_id}/discovery/v2.0/keys",
+            issuer=f"https://sts.windows.net/{tenant_id}/",
+            audience=client_id, 
+            algorithm="RS256"
+        )
+        mcp = FastMCP("GA4 Prod", auth=auth)
+        mcp.add_middleware(CustomHeaderMiddleware())
+        print("JWT Verifier created successfully (no audience)")
+    except Exception as e:
+        print(f"JWT Verifier creation failed: {e}")
+        mcp = FastMCP("GA4")
+        mcp.add_middleware(CustomHeaderMiddleware())
+
+else:
+    mcp = FastMCP("Production Server - No Config")
+    mcp.add_middleware(CustomHeaderMiddleware())
+
+
